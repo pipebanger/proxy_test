7e6d2936f298f25227dce916a9eadeea1392535a
pipebanger <ozzzmo@protonmail.com>
2025-08-14 13:41:15 +0500

    Minor fix

--- proxy_test.py
+++ proxy_test.py
@@ -3,7 +3,17 @@ import requests
 from urllib.parse import urlparse
 
 if len(sys.argv) != 2:
-    print('Использование: proxy_test.exe protocol://user:pass@address:port')
+    print(
+        'proxy_test v1.0.1\n'
+        'Simple utility to test proxy connections\n'
+        'Supports HTTP and SOCKS5 proxies.\n\n'
+        'Usage:\n'
+        '  proxy_test protocol://[user:password@]address:port\n'
+        '  (user and password are optional)\n\n'
+        'Examples:\n'
+        '  HTTP:   proxy_test http://john_doe:password@102.177.176.242:80\n'
+        '  SOCKS5: proxy_test socks5://38.127.179.125:46656'
+    )
     sys.exit(1)
 
 proxy_url = sys.argv[1]
@@ -19,14 +29,14 @@ proxies = {
 }
 
 test_url = 'http://httpbin.org/ip'
-
 try:
     response = requests.get(test_url, proxies=proxies, timeout=10)
     response_json = response.json()
     public_ip = response_json.get('origin')
     print(f' {address}:{port}\tПрокси работает. IP: {public_ip}')
-except requests.exceptions.RequestException as e:
 
+except requests.exceptions.RequestException as e:
     print(f' {address}:{port}\tОшибка прокси.')
+
 except ValueError:
     pass
